1596ef85e1158885b3708c73e8d9744e61dc583c
TSlash <45912519+TFlashgamer@users.noreply.github.com>
2020-07-20 20:26:28 +0300

    20.07 (added animation, fixed bugs)

--- js/languages.js
+++ js/languages.js
@@ -7,12 +7,12 @@ let rus = {
     projectheading: 'Вот несколько моих работ',
     code1: '<',
     code2: '/>',
-    projecttitle1: '< Сайт предназначен кому то />',
-    projecttitle2: '< Сайт с опросом />',
-    projecttitle3: '< Сайт лендинг для продаж продуктов />',
-    projecttitle4: '< Сайт справка />',
-    projecttitle5: '< Oni Discord бот />',
-    projecttitle6: '< В ближайшее время здесь будут новые работы... />',
+    projecttitle1: 'Сайт предназначен кому то',
+    projecttitle2: 'Сайт с опросом',
+    projecttitle3: 'Сайт лендинг для продаж продуктов',
+    projecttitle4: 'Сайт справка',
+    projecttitle5: 'Oni Discord бот',
+    projecttitle6: 'В ближайшее время здесь будут новые работы...',
     buttontext: 'Показать ещё',
     contactsheading: 'Нужно связаться со мной?',
     contactsdescription: 'Если возникли какие-то вопросы Вы можете воспользоваться ссылками ниже',
@@ -29,12 +29,12 @@ let ukr = {
     projectheading: 'Ось декілька моїх робіт',
     code1: '<',
     code2: '/>',
-    projecttitle1: '< Сайт призначений комусь />',
-    projecttitle2: '< Сайт з опитуванням />',
-    projecttitle3: '< Сайт лендинг для продажу продуктів />',
-    projecttitle4: '< Сайт довідка />',
-    projecttitle5: '< Oni Discord бот />',
-    projecttitle6: '< Найближчим часом тут з`являться нові роботи... />',
+    projecttitle1: 'Сайт призначений комусь',
+    projecttitle2: 'Сайт з опитуванням',
+    projecttitle3: 'Сайт лендинг для продажу продуктів',
+    projecttitle4: 'Сайт довідка',
+    projecttitle5: 'Oni Discord бот',
+    projecttitle6: 'Найближчим часом тут з`являться нові роботи...',
     buttontext: 'Показати ще',
     contactsheading: 'Потрібно зв`язатись зі мною?',
     contactsdescription: 'Якщо виникли якісь запитання Ви можете скористатись посиланнями нижче',
@@ -45,8 +45,11 @@ let ukr = {
 changeLagnuage();
 
 function changeLagnuage() {
-    let language = lang.checked ? ukr : rus;
-    document.querySelectorAll('[text]').forEach(el => {
-        el.innerHTML = language[el.getAttribute('text')];
-    })
+    setTimeout(function () {
+        let language = lang.checked ? ukr : rus;
+        document.querySelectorAll('[text]').forEach(el => {
+            el.innerHTML = language[el.getAttribute('text')];
+        })
+    }, 1000);
+
 }
